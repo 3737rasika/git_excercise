b4f7b87ecb7414ede46298e965a0905e24c57b15
Rasika <rasika9705@gmail.com>
2026-09-19 22:24:52 +0530

    created login page code

---
+++ client.py
@@ -0,0 +1,10 @@
+#127.0.0.1 is the loopback address (also called localhost) in IPv4.
+host='127.0.0.1'
+
+port = 1337
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+st = 'connection done'
+byt = st.encode()
+s.send(byt)
